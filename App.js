75ade1741cdd18d3bc0e09dff04307372a8508a6
tosebikan <tosebikan@gmail.com>
2020-11-29 06:01:36 +0000

    add redux library

--- App.js
+++ App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 
+const reducer = (state) => {
+  return state;
+};
+const store = createStore(reducer);
+
 function App() {
-  return <HomeScreen />;
+  return (
+    <Provider store={store}>
+      <HomeScreen />
+    </Provider>
+  );
 }
 
 export default App;
