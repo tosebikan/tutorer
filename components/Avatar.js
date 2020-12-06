437116d93d46b7c1af40c962e9e88683a0d69fa1
tosebikan <tosebikan@gmail.com>
2020-12-06 00:02:14 +0000

    get profile picture from api call

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,10 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Image, StyleSheet } from 'react-native';
 
 function Avatar() {
   const [photo, setPhoto] = useState(
     'https://health.utah.edu/themes/custom/theme_uou/img/placeholder-user.png'
   );
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api/')
+      .then((res) => res.json())
+      .then((res) => {
+        let profilePic = res.results[0].picture.thumbnail;
+        setPhoto(profilePic);
+      });
+  }, []);
 
   return <Image source={{ uri: photo }} style={styles.avatar} />;
 }
